f7ca858249f0ff4ceb4fe028e709a2353c11d1c6
TrexLaptop <juan.13jsg@gmail.com>
2024-10-15 12:51:46 -0500

    7.0 ADD second part py API, (ADD, FIND, UPDATE)

--- file.py
+++ file.py
@@ -14,8 +14,9 @@ COND_start = int(input("""
                        Que opcion desea 1. 
                        1. añadir
                        2. consultar
+                       3. actualizar
                        /> """))
-
+# Agregar en la DB 
 if  COND_start == 1:
     name = input("Digame su nombre: ")
     last_name = input("Digame su apellido: ")
@@ -28,13 +29,38 @@ if  COND_start == 1:
     mycursor.execute(query, val)
     mydb.commit()
     print(mycursor.rowcount, "registro(s) insertado(s) correctamente en ID", mycursor.lastrowid)
+    
+# Consultar en la DB 
 elif  COND_start == 2:
-    query = "SELECT name, age FROM users WHERE age ORDER BY age ASC;"
+    query = "SELECT * FROM users;"
     mycursor.execute(query)
     
     myresult = mycursor.fetchall()
     for row in myresult:
         print(row)
-
+        
+elif  COND_start == 3:
+    user_id = int(input("Dime su ID: "))
+    new_email = input("Digame su nuevo email:")
+    
+    
+    query1= "SELECT * FROM users WHERE user_id = %s"
+    query2 = "UPDATE users SET email =  %s WHERE user_id = %s"
+    
+    # Query1 find the row with that ID
+    mycursor.execute(query1, (user_id,))
+    myresult = mycursor.fetchall()
+    
+    # If the ID for the input exists in the DB
+    if(len(myresult) > 0):
+        # Query2 update the email related with the ID
+        mycursor.execute(query2, (new_email, user_id))
+        mydb.commit()
+        print(mycursor.rowcount, "Regsitro(s) acutalizado(s) correctamente en ID", mycursor.lastrowid)
+        
+        for row in myresult:
+            print(row)  
+    else:
+        print("Ese ID no existe en la DB")
 else:
-    print("Opcion no valida")
+    print("Opcion o valida")

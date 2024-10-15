df1f8a8f1c0760fdbce35f40322b73932a7a2f2e
TrexLaptop <juan.13jsg@gmail.com>
2024-10-15 13:07:34 -0500

    1.0 Start new branch for a new DB

--- Storage_Procedure_Depart.sql
+++
@@ -1,5 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE p_all_users(IN Depat_param VARCHAR(50))
-BEGIN 
-	SELECT * FROM usuarios WHERE departamento = Depat_param;
-END // 
--- View_libros_autores.sql
+++
@@ -1,7 +0,0 @@
--- VIEW 
-CREATE VIEW v_pais_autores AS
-SELECT autor, pais_autor 
-FROM libros
-WHERE pais_autor lIKE "A%";
-
-SELECT * FROM v_pais_autores

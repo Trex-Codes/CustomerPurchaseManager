ad2c5dad838e30cb737df40c5aa11ddff1d9bd0d
TrexLaptop <juan.13jsg@gmail.com>
2024-10-16 14:24:38 -0500

    4.0 Fixing issues with the login / Create a new account with info

--- Querys.sql
+++ Querys.sql
@@ -1,7 +1,7 @@
 SELECT * FROM cliente;
 SELECT * FROM tipo_producto;
 SELECT * FROM producto;
-SELECT * FROM venta;
+SELECT * FROM venta;	
 SELECT * FROM venta_producto;
 
 SELECT * 
--- file.py
+++ file.py
@@ -16,8 +16,9 @@ def Validation_email(email_patter):
     pattern = r"^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$" 
     return re.match(pattern,email_verify) is not None
 
+# Create new email (account) into the DB 
 def Create_email():
-        
+    
         # Request to the new user, the new information to create a row
         datos = ["nombre", "edad", "telefono", "ciudad", "direccion"]
         data_saved = []
@@ -34,12 +35,87 @@ def Create_email():
         mydb.commit()
         print(mycursor.rowcount, "Register(s) inserted in right way!")
         
+# Update personal information of the client that already have account 
+def  Update_info():
+
+    while True:
+        info_specific = int(input("""Which details you would to change it:
+                                    1. Legal name
+                                    2. phone number
+                                    3. email
+                                    4. city and address 
+                                    >/: 
+            
+                                """))
+        # update legal name
+        if info_specific == 1:
+                legal_nameChanged = input("Which is your new legal name: ")
+                
+                if legal_nameChanged == "":
+                    print("Enter a valid name")
+                else:
+                    query = "UPDATE cliente SET nombre = %s WHERE email = %s"
+                        
+                    mycursor.execute(query, (legal_nameChanged, email_verify))
+                    mydb.commit()   
+                    print("Your legal name has been successfully changed!!") 
+        
+        # update phone number p
+        if info_specific == 2:
+            phoneNumber_changed = input("Which is your new phone number (use indicative): ")
+            query = "UPDATE cliente SET telefono =  %s WHERE email = %s"
+            
+            mycursor.execute(query, (phoneNumber_changed, email_verify))
+            mydb.commit()
+            print("Your phone number has been successfully changed!!")
+
+        # update email address 
+        if  info_specific == 3:
+            email_changed = input("Which is your new email address: ")
+            
+            query_1 = "SELECT * from cliente  WHERE email = %s"
+            mycursor.execute(query_1, (email_changed,))
+            myresult = mycursor.fetchall()
+            
+            if (len(myresult) > 0):
+                print("This email is already in use!!, try another one")
+            else:
+                query_2 = "UPDATE cliente SET email = %s WHERE email = %s"
+            
+                mycursor.execute(query_2, (email_changed, email_verify))
+                mydb.commit()
+                print("Your email address has been successfully changed!!")
+                
+        # update city and address 
+        if  info_specific == 4:
+            city_changed = input("Which is your new city: ")
+            address_changed = input("Which is your new address: ")
+            
+            query_cityAddr = "UPDATE cliente SET ciudad = %s, direccion = %s WHERE email = %s;"
+            mycursor.execute(query_cityAddr,  (city_changed, address_changed, email_verify))
+            mydb.commit()
+            print("Your  city and address has been successfully changed!!")
+
+        if  info_specific == 5:
+            break;
+
+
+
+
+
+        
+                
+                
+
+        
+
 
 Messgae_Starting ="Welcome, please login with your credentials"
 print(Messgae_Starting.upper())
 
 # email request to verify the client
 email_verify = input("Email: ")
+# email_verify = ""
 Validation_email(email_verify)
 
 if Validation_email(email_verify):
@@ -55,10 +131,12 @@ if Validation_email(email_verify):
         print("Client Exist, now  you can login")
         login_acces_won = input("""Now which options you would like to do:
                                     1. Update your personal information
-                                    2. Delete your information3
+                                    2. Delete your information
                                     3. Find the list of products that you  have purchased
                                     4. Buy a new product -- 
                                 """)
+    if login_acces_won == "1":
+        Update_info()
     else:
         print("Client does not exist, please register first")
         Create_email() 

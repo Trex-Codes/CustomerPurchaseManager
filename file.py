642ced7197c28e542f6b59d13b44e68cf93b99f1
Trex-Codes <juan.13JSG@gmail.com>
2024-10-29 00:50:49 -0500

    11.0 FINISH PART FIND PRODUCTS AND BUY NEW PRODUCTS

--- file.py
+++ file.py
@@ -1,7 +1,13 @@
 import mysql.connector
 import re
 import os
+import sys
+import io
 import time
+from tabulate import tabulate
+
+# Configurar la codificación de salida a UTF-8
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 # Variable to update the email on file 
 global email_verify
@@ -158,79 +164,144 @@ def Delete_info():
         print("Your information has been successfully deleted!!")
         exit()
 
-
 # Find the products related with that specific client
 def Find_products():
-    
+    os.system('clear') 
     print("You will see all the products related with your information",'\n')
+    
+    # Find the ID of the cx to show the products that they have    
+    query_id = "SELECT cliente_id FROM cliente WHERE email = %s"
+    mycursor.execute(query_id,  (email_verify,))
+    result = mycursor.fetchall()
+    valor = result[0][0] # Get Value INT of the ID of the client
     
     query_product = """
 SELECT
 	cliente.cliente_id,
     cliente.nombre,
     venta.cantidad,  
+    venta_producto.venta_id AS "ID VENTA venta_producto",
+    venta_producto.producto_id AS "ID PRODUCTO venta_producto",
     producto.descripcion,
     producto.tipo_producto,
     categoria,
-    producto.precio
+    producto.precio,
+    SUM(producto.precio * venta.cantidad) AS "VALOR PAGADO"
 FROM cliente 
 JOIN venta ON cliente.cliente_id = venta.cliente_id
 JOIN venta_producto ON venta.venta_id = venta_producto.venta_id
 JOIN producto ON venta_producto.producto_id = producto.producto_id
-JOIN tipo_producto ON tipo_producto.tipo_producto_id = producto.producto_id;
-    """
-    
-    """
-    
-        Si deseo comprar algun producto debo primero:
-            1. comprar > llevaria a selccionar que tipo de producto 
-                luego el producto en especifico.
-                
-            2. finalizar la compra 
-            
-            ------------------------
-            
-            3. internamente tenemos que:
-                    - si ya existe una sesion asociado a un correo debemos 
-                    mostrar que tipo de productos existen y cual en especifico desea
-                    
-                    -     
-        
-    
-    """
+JOIN tipo_producto ON tipo_producto.tipo_producto_id = producto.tipo_producto
+WHERE cliente.cliente_id = %s
+GROUP BY 
+    cliente.cliente_id,
+    cliente.nombre,
+    venta_producto.venta_id,
+    venta_producto.producto_id,
+    producto.descripcion,
+    producto.tipo_producto,
+    tipo_producto.categoria,
+    producto.precio;"""
 
-
-    mycursor.execute(query_product)
+    mycursor.execute(query_product, (valor,))
     result_query = mycursor.fetchall()
 
     # name column required by Quey
     column_names = [i[0] for i in mycursor.description]
-        
-    print(column_names)
-    for row in result_query:
-        print(row)
-        
+    print(tabulate(result_query, headers=column_names, tablefmt="fancy_grid", floatfmt=".3f"))
+
     input("\n Press enter to go  back to the main menu...")
 
+# Buy a new product for the customer 
+def Buy_product():
+    def encapsulation_buyOptions(option_tipeProduct):
+        
+        os.system('clear')
+        print(f"you have select {option_tipeProduct}")
+        
+        # Query table (producto) with ID = x, products with that ID 
+        query_case = "SELECT producto_id, descripcion, precio FROM producto WHERE tipo_producto = %s;"
+        mycursor.execute(query_case, (option,))
+        result_case = mycursor.fetchall()
+    
+        print(tabulate(result_case, headers=['ID', 'Desccription', 'Cost'], tablefmt="fancy_grid", floatfmt=".3f"))
+    
+        # quantify and ID of the specific product in those variables    
+        product_choose = int(input("Tell me the ID of the product that you want: "))
+        product_choose_quanty = int(input("Tell me the Quantity of the product: "))
+        
+        """ 1. we need to add in venta (valor, product_choose_quanty) -- valor its the ID of the cx 
+            2. add to venta_producto el venta_id created for the new sell and the product ID for the cx selected
+      
+        """
+        
+        # SECTION 1 
+        #  Add to table (venta) the new sell with the customer ID and the value of the product
+        
+        query_ID_insert = "INSERT INTO venta (cliente_id, cantidad) VALUES (%s, %s);"
+        mycursor.execute(query_ID_insert, (valor, product_choose_quanty))
+        mydb.commit()
+        
+        # SECTION 2
+        # With a another Query we identify the venta_id of table (venta) that we need to add into venta_producto
+        # get the size of the tuples in the table 
+        
+        TUPLE_count = "SELECT COUNT(*) AS total_fila FROM venta;"
+        mycursor.execute(TUPLE_count)
+        result_case = mycursor.fetchall()
+            
+        result_TUPLE = result_case[0][0] # show a value type INT | The last venta_id  into the table
+        query_ID_insert_product = "INSERT INTO venta_producto (venta_id, producto_id) VALUES (%s, %s);"
+        mycursor.execute(query_ID_insert_product, (result_TUPLE, product_choose))
+        mydb.commit()
+                      
+    os.system('clear')
 
+    """
+        1. find  the ID of the client on the DB 
+        2. get the ID of the product that they want from Table (tipo_producto)
+        3. Depends of the option, they will see the table (producto) related with his ID 
+        4. in each case, we wil ask for the ID of specific product of table (producto) 
+           and also que quantity
+        
+        TO INSERT THE DATA ON DB
+        5. table (venta) we add the ID of the client and the quantity of the product 
+        6. table (venta_producto) we add the ID of the venta and the ID of the product 
+    """
+    
+    query_id = "SELECT cliente_id FROM cliente WHERE email = %s"
+
+    mycursor.execute(query_id,  (email_verify,))
+    result = mycursor.fetchall()
+    valor = result[0][0] # Get Value INT of the ID of the client
     
 
+    print(f"your ID it's {valor}")
+    print("Now we are going to show your the products: ","\n")
     
+    # Query types of products 
+    query_product = "SELECT * FROM tipo_producto;"
+    mycursor.execute(query_product)
+    result_product = mycursor.fetchall()
+
+    print(tabulate(result_product, headers=['ID', 'Category'], tablefmt="fancy_grid"))
+
+    # Input with function of to show the specifics products depending on the ID selected
+    print("\nnow select the category\n")
+    
+    option = int(input("Select option case: "))
+    Option_case = encapsulation_buyOptions(option) if 1 <= option <= 9 else "option not found" 
+    os.system('clear')
+    print(Option_case)
+    input("Enter to go back.. ")
 
 
-
-
-
-        
-                
-                
-
-        
-
+# ----------- START -----------          
 Messgae_Starting ="Welcome, please login with your credentials"
 print(Messgae_Starting.upper())
 
 # email request to verify the client
+# email_verify = "carlos@gmail.com"
 email_verify = input("Email: ")
 
 while True:
@@ -246,6 +317,7 @@ while True:
         
         # Validate if the  email is on the DB
         if len(result) >  0:
+            
             print("Client Exist, now  you can login")
             login_acces_won = input("""
                 Now which options you would like to do:
@@ -254,6 +326,7 @@ while True:
                 3. Find the list of products that you  have purchased
                 4. Buy a new product --
                 >/: """)
+            
             if login_acces_won == "1":
                 os.system('clear')
                 Update_info()
@@ -263,17 +336,15 @@ while True:
             elif  login_acces_won == "3":
                 os.system('clear')
                 Find_products()
+            elif login_acces_won == "4":
+                os.system('clear')
+                Buy_product()
             elif  login_acces_won == "5":
                 break;
         else:
             print("Option no valid, create an account")
             Create_email()
             break;
-        
     else:
         print("Email format is not valid")
         break
-        
-
-    
-    
--- prueba.py
+++ prueba.py
@@ -20,6 +20,9 @@ mycursor = mydb.cursor()
 
 qury_case = 0;
 result_case = 0;
+product_choose = 0;
+
+
 # mostar al cliente que ID es y al mismo tiempo ofrecerle los productos 
 
 email = "carlos@gmail.com"
@@ -32,18 +35,17 @@ result = mycursor.fetchall()
 valor = result[0][0]
 
 print(f"your ID it's {valor}")
-print("Now we are going to show your the products: ")
+print("Now we are going to show your the products: ","\n")
 
-# Query types of products 
-
+Query types of products 
 query_product = "SELECT * FROM tipo_producto;"
 mycursor.execute(query_product)
 result_product = mycursor.fetchall()
 
-# for row in result_product:
-    # print(row)
+for row in result_product:
+    print(row)
     
-print("now select the category")
+print("\nnow select the category\n")
 
 option = int(input("Select option case: "))
 
@@ -55,12 +57,31 @@ if option == 1:
     
     for i in result_case:
         print(i)
+        
+    product_choose = int(input("Tell me the ID of the product that you want: "))
+    product_choose_quanty = int(input("Tell me the Quantity of the product: "))
     
-elif option == 2:
-    print("you selected option 2")
-    query_case = "SELECT producto_id, descripcion, precio FROM producto WHERE tipo_producto = %s;"
-    mycursor.execute(query_case, (option,))
+    print("Product selected at your order!!, thaks")
+     
+    # INSERT DATA IN DB
+    # 1. we need to add in venta (valor, product_choose_quanty)
+    # 2. add to venta_producto el venta_id created for the new sell and the product id for the cx selected
+    
+    # SECTION 1 
+    query_ID_insert = "INSERT INTO venta (cliente_id, cantidad) VALUES (%s, %s);"
+    mycursor.execute(query_ID_insert, (valor, product_choose_quanty))
+    mydb.commit()
+    
+    # SECTION 2
+    
+    # get the size of the tuples in the table 
+    TUPLE_count = "SELECT COUNT(*) AS total_fila FROM venta;"
+    mycursor.execute(TUPLE_count)
     result_case = mycursor.fetchall()
+        
+    result_TUPLE = result_case[0][0]
+    print(type(result_TUPLE)) 
     
-    for i in result_case:
-        print(i)
+    query_ID_insert_product = "INSERT INTO venta_producto (venta_id, producto_id) VALUES (%s, %s);"
+    mycursor.execute(query_ID_insert_product, (result_TUPLE, product_choose))
+    mydb.commit()
